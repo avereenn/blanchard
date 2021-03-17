361bfacfaa3829e16aee0dd576e7714d2a2360e1
Igor Averin <awerig47@gmail.com>
2021-03-17 22:24:48 +0300

    header is stylized

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+`use strict`;
+
+const selectElems = document.querySelectorAll(`.js-select`);
+
+selectElems.forEach((select) => {
+  new Choices(select, {
+    searchEnabled: false,
+    itemSelectText: ``,
+  });
+});
