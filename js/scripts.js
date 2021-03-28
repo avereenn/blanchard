17ab190bf12d00a74633bba3d773c32df8f5ef81
Igor Averin <awerig47@gmail.com>
2021-03-28 14:55:45 +0300

    the header menu is stylized and a handler is added to the burger button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 `use strict`;
 
-const selectElems = document.querySelectorAll(`.js-select`);
+const selectElems = document.querySelectorAll(`.js-select`),
+      burgerBtnEl = document.querySelector(`.js-burger`),
+      menuEl = document.querySelector(`.js-menu`),
+      searchOpenBtnEl = document.querySelector(`.js-search-open`),
+      searchFoemEl = document.querySelector(`.js-search-form`);
 
 selectElems.forEach((select) => {
   new Choices(select, {
@@ -14,4 +18,10 @@ const selectListElems = document.querySelectorAll(`.header__select-item .choices
 
 selectListElems.forEach((list) => {
   new SimpleBar(list);
+});
+
+burgerBtnEl.addEventListener(`click`, function() {
+  this.classList.toggle(`header__burger_open`);
+  menuEl.classList.toggle(`header__menu_open`);
+  document.body.classList.toggle(`hold`);
 });
