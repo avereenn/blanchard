bce89201bb8d4ca5819f36e76cc0b04a11df0708
Igor Averin <awerig47@gmail.com>
2021-03-29 23:55:07 +0300

    the search form for breakpoint 1024 is stylized and the opening is implemented

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ const selectElems = document.querySelectorAll(`.js-select`),
       burgerBtnEl = document.querySelector(`.js-burger`),
       menuEl = document.querySelector(`.js-menu`),
       searchOpenBtnEl = document.querySelector(`.js-search-open`),
-      searchFoemEl = document.querySelector(`.js-search-form`);
+      searchFormEl = document.querySelector(`.js-search-form`);
 
 selectElems.forEach((select) => {
   new Choices(select, {
@@ -24,4 +24,19 @@ burgerBtnEl.addEventListener(`click`, function() {
   this.classList.toggle(`header__burger_open`);
   menuEl.classList.toggle(`header__menu_open`);
   document.body.classList.toggle(`hold`);
+});
+
+searchOpenBtnEl.addEventListener(`click`, function() {
+  this.classList.add(`header__search-open_hidden`);
+  searchFormEl.classList.add(`search-form_show`);
+  searchFormEl.elements.search.focus();
+});
+
+window.addEventListener(`click`, event => {
+  if(!searchFormEl.classList.contains(`search-form_show`) ||
+  event.target.closest(`.search-form_show`) ||
+  event.target === searchOpenBtnEl) return;
+
+  searchOpenBtnEl.classList.remove(`header__search-open_hidden`);
+  searchFormEl.classList.remove(`search-form_show`);
 });
