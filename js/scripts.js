6d558ddc840de8c34c0ee0990cb080ddc434691a
Igor Averin <awerig47@gmail.com>
2021-09-12 13:02:26 +0300

    change layout in catalog painter-cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,55 @@
 `use strict`;
 
 const selectElems = document.querySelectorAll(`.js-select`),
-      burgerBtnEl = document.querySelector(`.js-burger`),
-      menuEl = document.querySelector(`.js-menu`),
-      searchOpenBtnEl = document.querySelector(`.js-search-open`),
-      searchFormEl = document.querySelector(`.js-search-form`),
-      gallerySliderListEl = document.querySelector(`.js-gallery-slider-list`),
-      galleryModalEl = document.querySelector(`.gallery-modal`),
-      galleryModalImgEl = galleryModalEl.querySelector(`.gallery-modal__img`),
-      galleryModalClose = document.querySelector(`.gallery-modal__close`),
-      expandBtnEl = document.querySelector(`.js-expand-btn`),
-      eventsListEl = document.querySelector(`.js-events-list`);
+  burgerBtnEl = document.querySelector(`.js-burger`),
+  menuEl = document.querySelector(`.js-menu`),
+  searchOpenBtnEl = document.querySelector(`.js-search-open`),
+  searchFormEl = document.querySelector(`.js-search-form`),
+  gallerySliderListEl = document.querySelector(`.js-gallery-slider-list`),
+  galleryModalEl = document.querySelector(`.gallery-modal`),
+  galleryModalImgEl = galleryModalEl.querySelector(`.gallery-modal__img`),
+  galleryModalClose = document.querySelector(`.gallery-modal__close`),
+  expandBtnEl = document.querySelector(`.js-expand-btn`),
+  eventsListEl = document.querySelector(`.js-events-list`);
 
 // HEADER SELECTS CHOICES
-selectElems.forEach((select) => {
+selectElems.forEach(select => {
   new Choices(select, {
     searchEnabled: false,
     itemSelectText: ``,
-    shouldSort: false,
+    shouldSort: false
   });
 });
 
 // HEADER SELECTS SIMPLEBAR
-const selectListElems = document.querySelectorAll(`.header__select-item .choices__list--dropdown`);
+const selectListElems = document.querySelectorAll(
+  `.header__select-item .choices__list--dropdown`
+);
 
-selectListElems.forEach((list) => {
+selectListElems.forEach(list => {
   new SimpleBar(list);
 });
 
 // HEADER BURGER
-burgerBtnEl.addEventListener(`click`, function() {
+burgerBtnEl.addEventListener(`click`, function () {
   this.classList.toggle(`header__burger_open`);
   menuEl.classList.toggle(`header__menu_open`);
   document.body.classList.toggle(`hold`);
 });
 
 // HEADER SEARCH OPEN
-searchOpenBtnEl.addEventListener(`click`, function() {
+searchOpenBtnEl.addEventListener(`click`, function () {
   this.classList.toggle(`header__search-open_active`);
   searchFormEl.classList.toggle(`search-form_show`);
 });
 
 window.addEventListener(`click`, event => {
-  if(!searchFormEl.classList.contains(`search-form_show`) ||
-  event.target.closest(`.search-form_show`) ||
-  event.target === searchOpenBtnEl) return;
+  if (
+    !searchFormEl.classList.contains(`search-form_show`) ||
+    event.target.closest(`.search-form_show`) ||
+    event.target === searchOpenBtnEl
+  )
+    return;
 
   searchOpenBtnEl.classList.remove(`header__search-open_active`);
   searchFormEl.classList.remove(`search-form_show`);
@@ -56,35 +61,39 @@ const gallerySwiper = new Swiper(`.gallery-slider`, {
 
   pagination: {
     el: '.gallery-slider__pages',
-    type: `fraction`,
+    type: `fraction`
   },
 
   navigation: {
     nextEl: '.gallery-slider__btn_next',
-    prevEl: '.gallery-slider__btn_prev',
+    prevEl: '.gallery-slider__btn_prev'
   },
 
   slidesPerView: 1,
-  slidesPerColumn: 1,
-  slidesPerColumnFill: `row`,
+  slidesPerGroup: 1,
+  grid: {
+    rows: 1
+  },
   spaceBetween: 6,
 
   breakpoints: {
-
     500: {
       slidesPerView: 2,
-      slidesPerColumn: 2,
-      slidesPerColumnFill: `row`,
-      spaceBetween: 34,
+      slidesPerGroup: 2,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 34
     },
 
     1440: {
       slidesPerView: 3,
-      slidesPerColumn: 2,
-      slidesPerColumnFill: `row`,
-      spaceBetween: 50,
-    },
-
+      slidesPerGroup: 3,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 50
+    }
   }
 });
 
@@ -93,7 +102,7 @@ const gallerySwiper = new Swiper(`.gallery-slider`, {
 gallerySliderListEl.addEventListener(`click`, event => {
   event.preventDefault();
   const link = event.target.closest(`.gallery-slider__link`);
-  if(!link) return;
+  if (!link) return;
   const src = link.getAttribute(`href`);
 
   galleryModalEl.classList.add(`gallery-modal_open`);
@@ -104,8 +113,12 @@ gallerySliderListEl.addEventListener(`click`, event => {
   setTimeout(() => document.querySelector(`.js-bg-btn`).focus(), 160);
 });
 
-galleryModalEl.addEventListener(`click`, function(event) {
-  if(!event.target.matches(`.gallery-modal__bg-btn`) && !event.target.matches(`.gallery-modal__close`)) return;
+galleryModalEl.addEventListener(`click`, function (event) {
+  if (
+    !event.target.matches(`.gallery-modal__bg-btn`) &&
+    !event.target.matches(`.gallery-modal__close`)
+  )
+    return;
 
   this.classList.remove(`gallery-modal_open`);
   document.body.classList.remove(`hold`);
@@ -114,75 +127,69 @@ galleryModalEl.addEventListener(`click`, function(event) {
 // TABS
 $(`.js-catalog-tabs`).tabs({
   active: 2,
-  show: {duration: 160},
-  hide: {duration: 160},
+  show: { duration: 160 },
+  hide: { duration: 160 }
 });
 
 // ACCORDION
-$(`.js-accordion`).each(function() {
+$(`.js-accordion`).each(function () {
   $(this).accordion({
     collapsible: true,
     header: `.accordion__btn`,
-    heightStyle: `content`,
+    heightStyle: `content`
   });
 });
 
+// PAINTER-LINK
+$(`.js-catalog-tabs`).on(`click`, function(event) {
+  event.preventDefault();
+  if(!event.target.classList.contains(`accordion__painter-link`)) return;
+  alert(event.target.textContent);
+});
+
 //Обработчик кнопки "Все события"
-expandBtnEl.addEventListener(`click`, (event) => {
-  eventsListEl.classList.add(`events__list_open`);
-  event.target.hidden = true;
+expandBtnEl.addEventListener(`click`, function () {
+  eventsListEl.classList.add(`events__list_show`);
+  this.hidden = true;
 });
 
 // EVENTS SWIPER
 const eventsSwiper = new Swiper(`.events-slider`, {
-  observer: true,
-
   pagination: {
-    el: '.events-slider__pagination',
+    el: '.events-slider__pagination'
   },
 
   slidesPerView: 1,
   spaceBetween: 6,
-  slidesPerColumnFill: `row`,
 
   breakpoints: {
-
     500: {
       enabled: false,
-      spaceBetween: 50,
-    },
-
+      spaceBetween: ``
+    }
   }
 });
 
 //EDITIONS SWIPER
 
 const editionsSwiper = new Swiper(`.editions-slider`, {
-  observer: true,
-
   enabled: false,
 
   breakpoints: {
-
     500: {
       //enabled: true,
-    },
-
+    }
   }
 });
 
 //PROJECTS SWIPER
 
 const projectsSwiper = new Swiper(`.projects-slider`, {
-  observer: true,
-
   navigation: {
     nextEl: '.projects-slider__btn_next',
-    prevEl: '.projects-slider__btn_prev',
+    prevEl: '.projects-slider__btn_prev'
   },
 
   slidesPerView: 1,
-  spaceBetween: 6,
-  slidesPerColumnFill: `row`,
-
+  spaceBetween: 6
 });
